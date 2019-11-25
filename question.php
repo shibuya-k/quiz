e975d8c350bc34b19f463df0798423041706f25c
Kohei Shibuya <g017a1322@gmail.com>
2019-11-25 16:23:18 +0900

    implemented check answer value.

--- question.php
+++ question.php
@@ -1,7 +1,6 @@
 <?php
 require_once('common.php');
 
-
 $i=rand(0,count($question)-1);
 ?>
 <!DOCTYPE html>
@@ -16,7 +15,22 @@ $i=rand(0,count($question)-1);
         </h1>
         <?= $question[$i][0] ?>
         <form action="result.php">
-        <input type="hidden" name="qid" value="<?= $i?>"></input>
-        <input type="text" name="answer"></input>
+        <input type="hidden" name="qid" value="<?= $i ?>">
+        <input type="text" id="text-answer" name="answer">
+        <button id="btn-answer">回答</button>
+        </form>
+        <script>
+        document.querySelector("#btn-answer").addEventListener("click",(e)=>{
+        let answer=document.querySelector("#text-answer");
+        if( answer.value == ""){
+        alert("入力してください。");
+        answer.focus();
+        answer.style.backgroundColor="Pink";
+        e.preventDefault();
+        }else{
+        document.querySelector("#btn-answer").innerHTML="送信中";
+        }
+        });
+        </script>
     </body>
 </html>
--- result.php
+++ result.php
@@ -1,4 +1,5 @@
 <?php
+sleep(3);
 require_once('common.php');
 
 //引数(クエリー)を受け取る
@@ -6,7 +7,7 @@ $qid=isset($_GET['qid'])? $_GET['qid']:-1;
 $answer=$_GET['answer'];
 
 //validation
-if($qid ==-1|| is_numeric($qid)||((0<=$qid)&&($qid<count($question)))){
+if($qid ==-1|| !is_numeric($qid)||!((0<=$qid)&&($qid<count($question)))){
 	echo "エラー : $qid invalid";
 	exit(1);
 }
